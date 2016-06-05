909038f79e9cb94285177590e92144ccf19ead93
Chris Gaona <thebeloved88@sbcglobal.net>
2016-06-04 17:27:48 -0700

    Gets 5 recent tweets showing up from Twitter API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var Twitter = require('twitter');
+var twitterKeys = require('../../twitter-config.json');
+
+var client = new Twitter({
+  consumer_key: twitterKeys.consumer_key,
+  consumer_secret: twitterKeys.consumer_secret,
+  access_token_key: twitterKeys.access_token_key,
+  access_token_secret: twitterKeys.access_token_secret
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+
+  var params = {screen_name: 'chrissgaona', count: 5};
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
+    if (!error) {
+      res.send(tweets);
+    }
+  });
 });
 
 module.exports = router;
